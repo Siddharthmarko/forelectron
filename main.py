5f3bb2e9512c33ada81705e21ee8f8189b738726
Siddharthmarko <92375052+Siddharthmarko@users.noreply.github.com>
2022-04-08 13:17:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 # This program prints Hello, world!
+import win32api
+from flask import Flask, render_template
 
-print('Hello, world!')
+app = Flask(__name__)
+
+
+#Using the below, the popup message appears on the page load of index.html
+#0x00001000 - This makes the popup appear over the browser window
+@app.route('/')
+def pythonhtml():
+    win32api.MessageBox(0, 'You have just run a python script on the page load!', 'Running a Python Script via Javascript', 0x00001000)
+    return render_template('pythonhtml.html')
+
+#Using the below, the popup message appears when the button is clicked on the webpage.
+#0x00001000 - This makes the popup appear over the browser window
+@app.route('/test')
+def test():
+    win32api.MessageBox(0, 'You have just run a python script on the button press!', 'Running a Python Script via Javascript', 0x00001000)
+    return render_template('pythonhtml.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# print('Hello, world!')
